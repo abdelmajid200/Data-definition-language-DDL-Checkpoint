ba04ad82abb9d8e039fffb200df8140daa1fb8cd
abdelmajid200 <133158310+abdelmajid200@users.noreply.github.com>
2024-05-27 11:18:50 +0100

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,18 +1,18 @@
- Create Customer Table
+
 CREATE TABLE Customer (
     Customer_id VARCHAR(20) PRIMARY KEY,
     Customer_Name VARCHAR(20) NOT NULL,
     Customer_Tel NUMBER
 );
 
- Create Product Table
+
 CREATE TABLE Product (
     Product_id VARCHAR(20) PRIMARY KEY,
     Product_Name VARCHAR(20) NOT NULL,
     Price NUMBER CHECK (Price > 0)
 );
 
- Create Orders Table
+
 CREATE TABLE Orders (
     Customer_id VARCHAR(20),
     Product_id VARCHAR(20),
@@ -25,11 +25,11 @@ CREATE TABLE Orders (
         REFERENCES Product(Product_id)
 );
 
- Add Category Column to Product Table
+
 ALTER TABLE Product
 ADD Category VARCHAR(20);
 
- Add OrderDate Column to Orders Table with Default Value
+
 ALTER TABLE Orders
 ADD OrderDate DATE DEFAULT SYSDATE;
 
